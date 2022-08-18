2d8e822a8d6b545780bdb756601ab27af5d3e34b
JeanCGuedes <jean.carlo.guedes@gmail.com>
2022-08-18 16:08:07 -0300

    Estrutura Inicial

---
+++ src/main/java/com/jornaldio/jornalapi/ApiJornalDioApplication.java
@@ -0,0 +1,13 @@
+package com.jornaldio.jornalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJornalDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJornalDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jornaldio/jornalapi/ApiJornalDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jornaldio.jornalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJornalDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
